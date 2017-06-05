b76a71ddc73c0491a6cd08b36f33f5094f629cde
berdrigue.bongolo-beto <berdrigue.bongolo-beto@epitech.eu>
2017-06-06 00:42:11 +0200

    fix varg debug

--- basic_test.c
+++ basic_test.c
@@ -4,8 +4,8 @@ CU_INIT();
 
 TEST(hello_world_test)
 {
-  CU_ASSERT_STR("hello world", "hello world", ==, "Not match, must be 'hello world'");
-  CU_ASSERT_STR("hello world", "hello", !=, "Not match, must be 'hello'");
+  CU_ASSERT_STR_EQ("hello world", "hello world", "Not match, must be 'hello world'");
+  CU_ASSERT_STR_NE("hello world", "hello", "Not match, must be 'hello'");
 }
 
 TEST(number_42_test)
